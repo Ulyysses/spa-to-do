4617444682343096f7b52dc31bc773238efaa096
Uliana Sanina <sanina.ulyana111@gmail.com>
2023-10-03 21:36:08 +0400

    try to make useAppDispatch

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,9 @@
 import {
     TypedUseSelectorHook,
-    useSelector as selectorHook
+    useSelector as selectorHook,
+    useDispatch
   } from 'react-redux';
   import { RootState } from './store';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = selectorHook; 
+// export const useAppDispatch: () => AppDispatch = useDispatch
--- src/store.ts
+++ src/store.ts
@@ -4,3 +4,4 @@ import { rootReducer } from "./services/reducers/rootReducer";
 export const store = createStore(rootReducer);
 
 export type RootState = ReturnType<typeof store.getState>
+// export type AppDispatch = typeof store.dispatch
