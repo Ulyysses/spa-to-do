9bd8beed46579b721334b98adaeca86764ae354e
Uliana Sanina <sanina.ulyana111@gmail.com>
2023-10-05 19:17:32 +0400

    change name

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import MainPage from "../main-page/MainPage";
-import ProjectOne from "../project";
+import Project from "../project";
 
 const App = () => {
   return (
     <main className="container">
       <Routes>
         <Route path="/spa-to-do" element={<MainPage />} />
-        <Route path="/spa-to-do/my-first-project" element={<ProjectOne />} />
+        <Route path="/spa-to-do/my-first-project" element={<Project />} />
       </Routes>
     </main>
   );
